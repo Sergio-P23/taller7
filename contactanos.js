c82471f1a5f84bc72709ef17b4ac26d5134d597b
Sergio-P23 <patarroyoarias23@gmail.com>
2025-05-07 14:15:32 -0500

    funcionalidad de la pagina completa

--- contactanos.js
+++ contactanos.js
@@ -1,8 +1,8 @@
-contact_js = function enviar() {
-    let email = document.getElementById('email').value;
-    let msg = document.getElementById('msg').value;
-    alert("Gracias por contactarnos " + email);
-    document.getElementById('output').textContent = "Tu mensaje fue: " + msg;
+function enviar() {
+
+        event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
+        const email = document.getElementById('email').value;
+        const msg = document.getElementById('msg').value;
+        alert("Gracias por contactarnos " + email);
+        document.getElementById('output').textContent = "Tu mensaje fue: " + msg;
 }
-
-
