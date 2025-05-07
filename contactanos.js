a0c46392f45d7f9dde9fce44806cc9ff0d11355c
Sergio-P23 <patarroyoarias23@gmail.com>
2025-05-07 14:24:33 -0500

    incorporacion de bootstrap y sweetalert

--- contactanos.js
+++ contactanos.js
@@ -1,8 +1,12 @@
 function enviar() {
+    let nombre = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let mensaje = document.getElementById('msg').value;
 
-        event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
-        const email = document.getElementById('email').value;
-        const msg = document.getElementById('msg').value;
-        alert("Gracias por contactarnos " + email);
-        document.getElementById('output').textContent = "Tu mensaje fue: " + msg;
+    Swal.fire({
+        title: 'Mensaje Enviado',
+        html: `<b>Gracias ${nombre}</b><br>Te responderemos a <i>${email}</i>.`,
+        icon: 'success',
+        confirmButtonText: 'OK'
+    });
 }
